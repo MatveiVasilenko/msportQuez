c27e94602d869fa6b68fc87618f560f937f99c7b
MatveiVasilenko <mat.vasilenkoo@gmail.com>
2020-05-13 01:21:18 +0300

    commit

--- js/index.js
+++ js/index.js
@@ -11,12 +11,10 @@ window.onload = () => {
 		if (e.target.value == 'on') {
 			howMany.style.display = "block"
 			sportQuiz1.style.display = "block"
-			sportQuiz2.style.display = "block"
 			ifNoWhy.style.display = "none"
 			setTimeout(() => {
 				ifNoWhy.style.opacity = "0"
 				sportQuiz1.style.opacity = "1"
-				sportQuiz2.style.opacity = "1"
 				howMany.style.opacity = "1"
 			})
 		}
@@ -27,12 +25,40 @@ window.onload = () => {
 			ifNoWhy.style.display = "block"
 			howMany.style.display = "none"
 			sportQuiz1.style.display = "none"
-			sportQuiz2.style.display = "none"
 			setTimeout(() => {
 				ifNoWhy.style.opacity = "1"
 				sportQuiz1.style.opacity = "0"
-				sportQuiz2.style.opacity = "0"
 				howMany.style.opacity = "0"
+			})
+		}
+	}
+
+
+
+
+	const ifNoWhy2 = document.getElementById('ifNoWhy2')	
+
+	const yes2 = document.getElementById('yes2')
+	const no2= document.getElementById('no2')
+
+	yes2.onchange = (e) => {
+		if (e.target.value == 'on') {
+			sportQuiz2.style.display = "block"
+			ifNoWhy2.style.display = "none"
+			setTimeout(() => {
+				ifNoWhy2.style.opacity = "0"
+				sportQuiz2.style.opacity = "1"
+			})
+		}
+	}
+	no2.onchange = (e) => {
+		if (e.target.value == 'on') {
+			
+			ifNoWhy2.style.display = "block"
+			sportQuiz2.style.display = "none"
+			setTimeout(() => {
+				ifNoWhy2.style.opacity = "1"
+				sportQuiz2.style.opacity = "0"
 			})
 		}
 	}
